91dab51025306caee6ed5acbf8153732b7d9607f
Diana064 <dipustolyakova02@gmail.com>
2022-06-29 19:31:18 +0300

    add animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 document.getElementById('btnFirst').onclick = function () {
   document.getElementById('content__first').classList.toggle('content-option');
+  document.getElementById('first-content').classList.toggle('rotate-picture');
   this.classList.toggle('rotate');
 };
 document.getElementById('btnSecond').onclick = function () {
   document.getElementById('content__second').classList.toggle('content-option');
+  document.getElementById('second-content').classList.toggle('rotate-picture');
   this.classList.toggle('rotate');
 };
 document.getElementById('btnThird').onclick = function () {
   document.getElementById('content__third').classList.toggle('content-option');
+  document.getElementById('third-content').classList.toggle('rotate-picture');
   this.classList.toggle('rotate');
 };
